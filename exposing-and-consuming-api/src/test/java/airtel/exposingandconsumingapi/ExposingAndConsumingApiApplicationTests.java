581fe20ab15b1cfdb35884cdb9347f552e4ff4a6
tanmay17268 <36583698+tanmay17268@users.noreply.github.com>
2020-06-09 11:19:18 +0530

    Add files via upload

---
+++ exposing-and-consuming-api/src/test/java/airtel/exposingandconsumingapi/ExposingAndConsumingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package airtel.exposingandconsumingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposingAndConsumingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
